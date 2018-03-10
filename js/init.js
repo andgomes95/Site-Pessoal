f430d5278f8586d6201906a7436b882f37e8ad44
André Gomes <andgomes95@gmail.com>
2018-03-10 20:42:44 -0300

    Edição da pagina Preferencias, adicionando o collapsible e imagens

--- js/init.js
+++ js/init.js
@@ -4,6 +4,13 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.carousel.carousel-slider').carousel({fullWidth: true});
+    $(document).ready(function(){
+	  $('.collapsible').collapsible();
+	});
+	$(document).ready(function(){
+      $('.carousel').carousel();
+    });
+
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
